8bd8aa372ac9fe342ae4ddffb3c8e915f5d2a326
Daniel Steffy <steffyd@users.noreply.github.com>
2023-03-29 11:06:43 -0600

    added a buncha dbt stuff and integrated into dagster

--- dagster-code/nhl/assets/__init__.py
+++ dagster-code/nhl/assets/__init__.py
@@ -1,6 +1,13 @@
 from dagster import load_assets_from_package_module
+from dagster._utils import file_relative_path
+from dagster_dbt import load_assets_from_dbt_project
 from . import nhl_ingestion
+
+DBT_PROJECT_DIR = file_relative_path(__file__, "../nhl_dbt")
+DBT_PROFILES_DIR = DBT_PROJECT_DIR + "/config"
 
 nhl_ingestion_assets = load_assets_from_package_module(
     package_module=nhl_ingestion, group_name="nhl_ingestion"
 )
+
+dbt_assets = load_assets_from_dbt_project(DBT_PROJECT_DIR)
---
+++ dagster-code/nhl/nhl_dbt/models/clean/game_outcomes.sql
@@ -0,0 +1,31 @@
+WITH ranked_teams AS (
+  SELECT
+    game_id,
+    team,
+    goals,
+    ROW_NUMBER() OVER (PARTITION BY game_id ORDER BY goals DESC) as rank
+  FROM {{source('raw_data', 'game_data')}}
+),
+no_ties as (
+    select gd1.game_id
+    from {{source('raw_data', 'game_data')}} gd1
+    join {{source('raw_data', 'game_data')}} gd2 on gd1.game_id = gd2.game_id
+    where gd1.goals <> gd2.goals
+)
+SELECT
+  rt.game_id,
+  rt.team,
+  case when nt.game_id is null then 'tied'
+    else case rank
+      when 1 then 'winner'
+       else 'loser' end
+  end as team_type,
+  rt.goals
+FROM ranked_teams rt
+left join no_ties nt on rt.game_id = nt.game_id
+group by rt.game_id, rt.team, case when nt.game_id is null then 'tied'
+    else case rank
+      when 1 then 'winner'
+       else 'loser' end
+  end, rt.goals
+order by rt.game_id, team_type
