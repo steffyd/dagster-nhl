b1b364d790ec07915f8a33ee7961c77703d672d7
Daniel Steffy <steffyd@users.noreply.github.com>
2023-04-24 10:18:38 -0600

    add asset reconciliation sensor for the clean group

---
+++ dagster-code/nhl/sensors/asset_sensors.py
@@ -0,0 +1,7 @@
+from dagster import build_asset_reconciliation_sensor, AssetSelection
+
+clean_assets_sensor = build_asset_reconciliation_sensor(
+    AssetSelection.groups("clean"),
+    name="clean_assets_sensor",
+    description="Sensor to keep clean assets up to date with the raw assets",
+)
