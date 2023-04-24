cdc585b0d76cb527a42a7f5d1513f8613596a0c5
Daniel Steffy <steffyd@users.noreply.github.com>
2023-04-24 10:22:40 -0600

    add clean_asset_sensor to sensors

--- dagster-code/nhl/sensors/__init__.py
+++ dagster-code/nhl/sensors/__init__.py
@@ -1,3 +1,4 @@
 from .slack_sensors import slack_on_run_failure
+from .asset_sensors import clean_assets_sensor
 
-SENSORS = [slack_on_run_failure]
+SENSORS = [slack_on_run_failure, clean_assets_sensor]
