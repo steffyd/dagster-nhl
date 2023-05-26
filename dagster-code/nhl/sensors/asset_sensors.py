b27c914842e5c8e79a39f2eba77db184e136d2df
Daniel Steffy <steffyd@users.noreply.github.com>
2023-05-26 14:32:06 -0600

    Add morph asset sensor and retool a dbt model

--- dagster-code/nhl/sensors/asset_sensors.py
+++ dagster-code/nhl/sensors/asset_sensors.py
@@ -5,3 +5,9 @@ clean_assets_sensor = build_asset_reconciliation_sensor(
     name="clean_assets_sensor",
     description="Sensor to keep clean assets up to date with the raw assets",
 )
+
+morph_assets_sensor = build_asset_reconciliation_sensor(
+    AssetSelection.groups("morph"),
+    name="clean_assets_sensor",
+    description="Sensor to keep clean assets up to date with the raw assets",
+)
