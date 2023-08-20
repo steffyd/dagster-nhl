6bf279b12c45e621eaaca9daf9531b4c21e03f61
Daniel Steffy <steffyd@users.noreply.github.com>
2023-08-20 11:22:16 -0600

    removed old reconciliation sensors

--- dagster-code/nhl/sensors/__init__.py
+++ dagster-code/nhl/sensors/__init__.py
@@ -1,4 +1,3 @@
 from .slack_sensors import slack_on_run_failure
-from .asset_sensors import clean_assets_sensor
 
-SENSORS = [slack_on_run_failure, clean_assets_sensor]
+SENSORS = [slack_on_run_failure]
