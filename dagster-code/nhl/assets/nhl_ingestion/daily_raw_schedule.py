90a71f34966c8b6c1b56b20fb64c04235e3ceefe
Daniel Steffy <steffyd@users.noreply.github.com>
2023-08-20 12:08:53 -0600

    fixed a couple things

--- dagster-code/nhl/assets/nhl_ingestion/daily_raw_schedule.py
+++ dagster-code/nhl/assets/nhl_ingestion/daily_raw_schedule.py
@@ -4,14 +4,14 @@ from datetime import timedelta
 from utils.nhl_api import get_schedule_expanded
 from utils.utils import get_partition_time_range
 from assets.partitions import nhl_future_week_daily_partition
+from math import floor
 
 
 @asset(
         io_manager_key="warehouse_io_manager",
         partitions_def=nhl_future_week_daily_partition,
         compute_kind="api",
-        metadata={"partition_expr": "TIMESTAMP_SECONDS(game_date)"},
-        key_prefix="nhl_ingestion"
+        metadata={"partition_expr": "TIMESTAMP_SECONDS(game_date)"}
 )
 def schedule_raw(context):
     start_time, end_time = get_partition_time_range(context)
@@ -25,8 +25,8 @@ def schedule_raw(context):
     while start_time <= end_time:
         # add the schedule for the day to the schedules dataframe
         schedules = pd.concat([schedules,get_schedule_expanded(start_time.strftime('%Y-%m-%d'), context)], ignore_index=True)
-        # only log out every 10% of the schedule
-        if count/total_days * 100 % 10 == 0:
+        # only log out near 10% of the schedule
+        if floor(count/total_days * 100) % 10 == 0:
             context.log.info(f"Retrieved schedule for {count} days")
         start_time = start_time + timedelta(days=1)
         count += 1
