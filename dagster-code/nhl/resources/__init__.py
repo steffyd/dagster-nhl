ff74ea56c9c69b0298299bd41d0e5c8e21210db7
Daniel Steffy <steffyd@users.noreply.github.com>
2023-08-22 12:27:17 -0600

    update the location of the profiles

--- dagster-code/nhl/resources/__init__.py
+++ dagster-code/nhl/resources/__init__.py
@@ -3,7 +3,7 @@ from .postgres_partitioned_io_manager import postgres_partitioned_io_manager
 from .coc_slack_resource import coc_slack_resource
 from dagster import EnvVar
 from dagster_dbt import DbtCliResource
-from utils.constants import DBT_PROJECT_DIR
+from utils.constants import DBT_PROJECT_DIR, DBT_PROFILES_DIR
 from dagster_gcp_pandas import BigQueryPandasIOManager
 import os
 
@@ -11,7 +11,7 @@ RESOURCES = {
     "postgres_resource_by_db": postgres_resource_by_db,
     "postgres_partitioned_io_manager": postgres_partitioned_io_manager,
     "coc_slack_resource": coc_slack_resource,
-    "dbt": DbtCliResource(project_dir=os.fspath(DBT_PROJECT_DIR), profile="prod"),
+    "dbt": DbtCliResource(project_dir=os.fspath(DBT_PROJECT_DIR), profiles_dir=DBT_PROFILES_DIR, profile="prod"),
     "warehouse_io_manager": BigQueryPandasIOManager(
         project="corellian-engineering-co",
         dataset="NHLData",
