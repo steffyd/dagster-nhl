0bcea58efa9b163b927efb24ca1f804d04a4cdcf
Daniel Steffy <steffyd@users.noreply.github.com>
2023-08-22 14:46:23 -0600

    fix things

--- dagster-code/nhl/assets/nhl_ingestion/game_data_raw.py
+++ dagster-code/nhl/assets/nhl_ingestion/game_data_raw.py
@@ -1,9 +1,8 @@
-import datetime as dt
 import pandas as pd
 from dagster import asset, Output
 from utils.nhl_api import get_team_stats
 from assets.partitions import nhl_daily_partition
-
+from utils.utils import is_closest_to_ten_percent
 
 @asset(
         io_manager_key="warehouse_io_manager",
@@ -17,13 +16,17 @@ def game_data_raw(context, schedule_raw: pd.DataFrame):
     # call the get_team_stats function for each game
     # and add the results to the game_data dataframe
     game_data = pd.DataFrame()
+    context.log.info(f"Retrieving game data for {len(schedule_raw)} games")
     for index, row in schedule_raw.iterrows():
-        team_stats_for_game = get_team_stats(row["game_id"], context)
+        team_stats_for_game = get_team_stats(row["game_id"])
         team_stats_for_game["game_date"] = row["game_date"]
         team_stats_for_game["game_type"] = row["game_type"]
         team_stats_for_game["season"] = row["season"]
         game_data = pd.concat([game_data, team_stats_for_game], ignore_index=True)
-
+        # only log out near 10% of the schedule
+        if is_closest_to_ten_percent(len(schedule_raw), index):
+            context.log.info(f"Retrieved game data for {index} games")
+            
     if not game_data.empty:
         yield Output(game_data, metadata={"game_data_count":len(game_data)})
     else:
--- dagster-code/nhl/utils/utils.py
+++ dagster-code/nhl/utils/utils.py
@@ -1,5 +1,6 @@
 from datetime import datetime, timedelta
 from dagster import OpExecutionContext
+from math import floor
 
 # helper function that takes in an OpExecutionContext
 # and returns the datetimes for a partition starttime and endtime
@@ -15,3 +16,10 @@ def get_partition_time_range(context : OpExecutionContext) -> (datetime, datetim
     context.log.info({"start_time": query_start_time.strftime('%Y-%m-%d'),
                         "end_time": query_end_time.strftime('%Y-%m-%d')})
     return query_start_time, query_end_time
+
+
+# write a utility that takes in a total value and a current value
+# and returns True if the current value is the closest possible value
+# to a multiple of 10% of the total value
+def is_closest_to_ten_percent(total, current):
+    return abs(current - (floor(current/total * 100) * total/100)) < abs(current - (floor(current/total * 100) + 1) * total/100)
