bd0992013ee0c4970c34ee041b65f597acbe1fb6
Daniel Steffy <steffyd@users.noreply.github.com>
2023-08-23 12:04:23 -0600

    update translator

--- dagster-code/nhl/assets/dbt_assets/NHLDagsterDbtTranslator.py
+++ dagster-code/nhl/assets/dbt_assets/NHLDagsterDbtTranslator.py
@@ -7,7 +7,7 @@ from dagster_dbt import DagsterDbtTranslator
 class NHLDagsterDbtTranslator(DagsterDbtTranslator):
     @classmethod
     def get_asset_key(cls, dbt_resource_props: Mapping[str, Any]) -> AssetKey:
-        asset_key = super().get_asset_key(dbt_resource_props)
+        asset_key = super().get_asset_key(dbt_resource_props).path
         if len(asset_key) > 1:
             # take the prefix from the asset key and add it
             # as a string to the asset key with a _ separator
