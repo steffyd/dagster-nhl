a4199afd23fc95352f96f42b3b16ff392f4f9a2d
Daniel Steffy <steffyd@users.noreply.github.com>
2023-09-06 15:24:43 -0600

    fix something

--- dagster-code/nhl/assets/dbt_assets/dbt_assets.py
+++ dagster-code/nhl/assets/dbt_assets/dbt_assets.py
@@ -9,24 +9,24 @@ from .NHLDagsterDbtTranslator import NHLDagsterDbtTranslator
 
 
 
-@dbt_assets(
-    manifest=Path(DBT_PROJECT_DIR, "target", "manifest.json"),
-    partitions_def=nhl_daily_partition,
-    dagster_dbt_translator=NHLDagsterDbtTranslator(),
-    select="game_data"
-)
-def dbt_assets(context: OpExecutionContext, dbt: DbtCliResource):
-    time_window = context.asset_partitions_time_window_for_output(
-        list(context.selected_output_names)[0]
-    )
+# @dbt_assets(
+#     manifest=Path(DBT_PROJECT_DIR, "target", "manifest.json"),
+#     partitions_def=nhl_daily_partition,
+#     dagster_dbt_translator=NHLDagsterDbtTranslator(),
+#     select="game_data"
+# )
+# def dbt_assets(context: OpExecutionContext, dbt: DbtCliResource):
+#     time_window = context.asset_partitions_time_window_for_output(
+#         list(context.selected_output_names)[0]
+#     )
 
-    dbt_vars = {
-        "min_date": time_window.start.strftime('%Y-%m-%d'),
-        "max_date": time_window.end.strftime('%Y-%m-%d')
-    }
-    dbt_build_args = ["build", "--vars", json.dumps(dbt_vars)]
+#     dbt_vars = {
+#         "min_date": time_window.start.strftime('%Y-%m-%d'),
+#         "max_date": time_window.end.strftime('%Y-%m-%d')
+#     }
+#     dbt_build_args = ["build", "--vars", json.dumps(dbt_vars)]
 
-    yield from dbt.cli(dbt_build_args, context=context).stream()
+#     yield from dbt.cli(dbt_build_args, context=context).stream()
 
 @dbt_assets(
     manifest=Path(DBT_PROJECT_DIR, "target", "manifest.json"),
