f21a1f29395dc794d686701cfa7f16434f516b1c
Daniel Steffy <steffyd@users.noreply.github.com>
2023-12-21 16:10:51 -0800

    add init for the new assets

--- dagster-code/sports-code/assets/__init__.py
+++ dagster-code/sports-code/assets/__init__.py
@@ -2,6 +2,7 @@ from dagster import load_assets_from_package_module
 from . import nhl_ingestion
 from . import dbt_assets
 from . import nfl_ingestion
+from . import general_sports
 
 
 nhl_ingestion_assets = load_assets_from_package_module(
@@ -10,6 +11,8 @@ nhl_ingestion_assets = load_assets_from_package_module(
 
 nfl_ingestions_assets = load_assets_from_package_module(package_module=nfl_ingestion, group_name="nfl_ingestion")
 
+general_sports_assets = load_assets_from_package_module(package_module=general_sports, group_name="general_sports")
+
 dbt_assets = load_assets_from_package_module(dbt_assets, group_name="dbt_assets")
 
-ASSETS = [*nhl_ingestion_assets, *dbt_assets, *nfl_ingestions_assets]
+ASSETS = [*nhl_ingestion_assets, *dbt_assets, *nfl_ingestions_assets, *general_sports_assets]
