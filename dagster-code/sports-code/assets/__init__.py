5da941cf41b24d544adaa61123b74425a4c417e7
Danny Steffy <danny.steffy@gmail.com>
2024-05-26 22:03:07 +0000

    remove general_sports as well

--- dagster-code/sports-code/assets/__init__.py
+++ dagster-code/sports-code/assets/__init__.py
@@ -1,6 +1,2 @@
-from dagster import load_assets_from_package_module
-from . import general_sports
 
-general_sports_assets = load_assets_from_package_module(package_module=general_sports, group_name="general_sports")
-
-ASSETS = [*general_sports_assets]
+ASSETS = []
