7ec92628bb0ca6d5fedaee39b185163945e71a19
Danny Steffy <danny.steffy@gmail.com>
2024-05-26 23:24:14 +0000

    add vscode devcontainer

--- dagster-code/sports-code/assets/ingest/nhl_games.py
+++ dagster-code/sports-code/assets/ingest/nhl_games.py
@@ -1,1 +1,10 @@
-from dagster import asset
+from dagster import asset, AssetExecutionContext
+from dagster_gcp import GCSResource
+import requests
+from ..partitions import nhl_daily_partition
+from utils.nhl_api import get_schedule
+
+@asset(partitions_def=nhl_daily_partition)
+def nhl_game_data(context: AssetExecutionContext, gcs: GCSResource):
+    
+    pass
--- dagster-code/sports-code/resources/__init__.py
+++ dagster-code/sports-code/resources/__init__.py
@@ -2,24 +2,16 @@ from .postgres_resource import postgres_resource_by_db
 from .postgres_partitioned_io_manager import postgres_partitioned_io_manager
 from .coc_slack_resource import coc_slack_resource
 from dagster import EnvVar
-from dagster_dbt import DbtCliResource
-from utils.constants import DBT_PROJECT_DIR, DBT_PROFILES_DIR, GCP_PROJECT_ID
-from dagster_gcp_pandas import BigQueryPandasIOManager
+from utils.constants import GCP_PROJECT_ID
 from dagster_gcp import BigQueryResource, GCSResource
-import os
-from resources.espn_api_resource import EspnApiResource
+from .espn_api_resource import EspnApiResource
 
 RESOURCES = {
     "postgres_resource_by_db": postgres_resource_by_db,
     "postgres_partitioned_io_manager": postgres_partitioned_io_manager,
     "coc_slack_resource": coc_slack_resource,
-    "dbt": DbtCliResource(project_dir=os.fspath(DBT_PROJECT_DIR), profiles_dir=DBT_PROFILES_DIR, profile="nhl_dbt"),
-    "warehouse_io_manager": BigQueryPandasIOManager(
-        project=GCP_PROJECT_ID,
-        dataset="NHLData",
-        gcp_credentials=EnvVar("GCP_CREDS")
-    ),
-    "bigquery": BigQueryResource(project=GCP_PROJECT_ID),
+    "bigquery": BigQueryResource(project=GCP_PROJECT_ID,
+                                 gcp_credentials=EnvVar("GCP_CREDS")),
     "gcs": GCSResource(project=GCP_PROJECT_ID),
     "espn_api": EspnApiResource(),
 }
