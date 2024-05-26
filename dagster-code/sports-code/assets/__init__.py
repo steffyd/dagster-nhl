b7e2690cd3de515ba7c031e82b88ca8898e7beac
Danny Steffy <danny.steffy@gmail.com>
2024-05-26 16:16:53 +0000

    Cleanup old code

--- dagster-code/sports-code/assets/__init__.py
+++ dagster-code/sports-code/assets/__init__.py
@@ -1,18 +1,6 @@
 from dagster import load_assets_from_package_module
-from . import nhl_ingestion
-from . import dbt_assets
-from . import nfl_ingestion
 from . import general_sports
-
-
-nhl_ingestion_assets = load_assets_from_package_module(
-    package_module=nhl_ingestion, group_name="nhl_ingestion"
-)
-
-nfl_ingestions_assets = load_assets_from_package_module(package_module=nfl_ingestion, group_name="nfl_ingestion")
 
 general_sports_assets = load_assets_from_package_module(package_module=general_sports, group_name="general_sports")
 
-dbt_assets = load_assets_from_package_module(dbt_assets, group_name="dbt_assets")
-
-ASSETS = [*nhl_ingestion_assets, *dbt_assets, *nfl_ingestions_assets, *general_sports_assets]
+ASSETS = [*general_sports_assets]
--- dagster-code/sports-code/nhl_dbt/not_ready_models/team_pythagorean_expectation_windowed.sql
+++
@@ -1,7 +0,0 @@
-{{
-    config(
-        materialized='incremental',
-        incremental_strategy='delete+insert',
-        unique_key='game_id'
-    )
-}}
