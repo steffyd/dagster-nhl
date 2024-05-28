ddeac823f7aa0f277cebcdec823c821bad9672ab
Danny Steffy <danny.steffy@gmail.com>
2024-05-28 20:33:12 +0000

    buncha changes

--- dagit/build.n.publish.bat
+++ dagit/build.n.publish.bat
@@ -1,2 +1,2 @@
-docker build -t steffyd/dagster_dagit:latest .
+docker build --platform=linux/amd64 --no-cache -t steffyd/dagster_dagit:latest .
 docker push steffyd/dagster_dagit:latest
--- dagster-code/build.n.publish.bat
+++ dagster-code/build.n.publish.bat
@@ -1,2 +1,2 @@
-docker build -t steffyd/dagster_code:latest .
+docker build --platform=linux/amd64 --no-cache -t steffyd/dagster_code:latest .
 docker push steffyd/dagster_code:latest
