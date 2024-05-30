677114cfc4a3c5f7d4f37a9a856546f5fbc76614
Daniel Steffy <steffyd@users.noreply.github.com>
2024-05-30 22:08:21 +0000

    update partition start date

--- dagster-code/sports-code/assets/partitions.py
+++ dagster-code/sports-code/assets/partitions.py
@@ -1,4 +1,4 @@
 from dagster import DailyPartitionsDefinition, WeeklyPartitionsDefinition
 
 
-nhl_weekly_partition = WeeklyPartitionsDefinition(start_date="1990-09-04", timezone=f"US/Pacific")
+nhl_weekly_partition = WeeklyPartitionsDefinition(start_date="1990-09-02", timezone=f"US/Pacific")
