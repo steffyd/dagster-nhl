8ebad11b136c0abeb8e8c61d7f9331f24672da36
Daniel Steffy <steffyd@users.noreply.github.com>
2024-05-31 20:31:45 +0000

    debug some stuff

--- dagster-code/sports-code/assets/ingest/nhl_games.py
+++ dagster-code/sports-code/assets/ingest/nhl_games.py
@@ -69,6 +69,7 @@ def nhl_game_data(context: AssetExecutionContext):
 def latest_nhl_schema(context: AssetExecutionContext, nhl_game_data: dict):
     # get the first game data to deduce the schema
     game_id, game_data = nhl_game_data.popitem()
+    context.log.info(game_data)
     generator = SchemaGenerator()
     schema_map, error_logs = generator.deduce_schema(game_data)
     schema = generator.flatten_schema(schema_map)
