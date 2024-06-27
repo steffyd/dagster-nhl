a9514c348344909c67da248265f4be5568370ab6
Daniel Steffy <steffyd@users.noreply.github.com>
2024-06-27 03:29:11 +0000

    update gcs io manager

--- dagster-code/sports-code/resources/partitioned_gcs_io_manager.py
+++ dagster-code/sports-code/resources/partitioned_gcs_io_manager.py
@@ -12,19 +12,21 @@ class PartitionedGCSIOManager(ConfigurableIOManager):
         path += f"/{partition}/{gameId}.json"
         return self.client.get_client().bucket(self.bucket).blob(path)
     
-    def _get_blobs(self, context):
-        path = "/".join(context.asset_key.path)
-        path += f"/{context.asset_partition_key}/"
+    def _get_blobs(self, asset_key_path, partition_key):
+        path = "/".join(asset_key_path)
+        path += f"/{partition_key}/"
         return self.client.get_client().bucket(self.bucket).list_blobs(prefix=path)
 
     def load_input(self, context: InputContext):
-        # we have multiple game data files to load for any given date,
-        # lets return the gcs blob for each gameId
-        blobs = self._get_blobs(context)
+        # check the number of partitions we're trying to load
         game_data = {}
-        for blob in blobs:
-            gameId = blob.name.split('/')[-1].split('.')[0]
-            game_data[(context.asset_partition_key, gameId)] = json.loads(blob.download_as_string())
+        for partition_keys in context.asset_partition_keys:
+            # we have multiple game data files to load for any given date,
+            # lets return the gcs blob for each gameId
+            blobs = self._get_blobs(context.asset_key.path,partition_keys)
+            for blob in blobs:
+                gameId = blob.name.split('/')[-1].split('.')[0]
+                game_data[(context.asset_partition_key, gameId)] = json.loads(blob.download_as_string())
         return game_data
     
     def handle_output(self, context: OutputContext, obj):
