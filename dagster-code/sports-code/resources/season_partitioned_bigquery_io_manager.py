5b0dc361493df630abbcc6f3258fc87de32bd627
Daniel Steffy <steffyd@users.noreply.github.com>
2024-06-27 20:31:26 +0000

    fix moar shite

--- dagster-code/sports-code/resources/season_partitioned_bigquery_io_manager.py
+++ dagster-code/sports-code/resources/season_partitioned_bigquery_io_manager.py
@@ -9,7 +9,7 @@ class SeasonPartitionedBigQueryIOManager(ConfigurableIOManager):
         with self.bq_resource.get_client() as client:
             # client is a google.cloud.bigquery.Client
             # table name is {asset_key}_{partition_key}
-            table_id = f"{context.asset_key.path}_{context.asset_partition_key}"
+            table_id = f"{'_'.join(context.asset_key.path)}_{context.asset_partition_key}"
             table = client.get_table(table_id)
             rows = client.list_rows(table)
             return [row for row in rows]
@@ -20,7 +20,7 @@ class SeasonPartitionedBigQueryIOManager(ConfigurableIOManager):
         with self.bq_resource.get_client() as client:
             # client is a google.cloud.bigquery.Client
             # table name is {asset_key}_{partition_key}
-            table_id = f"{context.asset_key.path}_{context.asset_partition_key}"
+            table_id = f"{'_'.join(context.asset_key.path)}_{context.asset_partition_key}"
             job_config = bigquery.LoadJobConfig(
                 autodetect=True,
                 write_disposition="WRITE_TRUNCATE",
