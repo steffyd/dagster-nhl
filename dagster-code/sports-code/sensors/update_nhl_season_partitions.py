1e0dd9ddeb600091df1ef772aecfc6ee72191c95
Daniel Steffy <steffyd@users.noreply.github.com>
2024-06-27 18:17:52 +0000

    quit being dumb python

--- dagster-code/sports-code/sensors/update_nhl_season_partitions.py
+++ dagster-code/sports-code/sensors/update_nhl_season_partitions.py
@@ -20,7 +20,7 @@ def update_nhl_season_partitions(context: SensorEvaluationContext):
     for season_id in season_ids:
         if season_id not in context.instance.get_dynamic_partitions(nhl_season_partition.name):
             context.log.info(f"Adding season partition {season_id}")
-            new_seasons.append(season_id)
+            new_seasons.append(str(season_id))
 
 
     context.log.info(f"Adding {len(new_seasons)} season partitions")
